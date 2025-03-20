ab1c744df1f8f8d1ae614212eac0930ce7acb257
Luciano Badilla <lucianobadilla88@gmail.com>
2025-03-20 12:56:41 -0300

    Agregar asistencia manual, con dos marcas agregada

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,9 +1,10 @@
-@props(['title','content','icon', 'clickeable' => false, 'route' => null])
+@props(['title', 'content', 'icon', 'clickeable' => false, 'route' => null, 'class' => ''])
 
-<a @isset($clickeable) href="{{ $route }}" @endisset
-    class="estado relative block overflow-hidden bg-white border border-black rounded-xl p-3 hover:text-black w-25 shadow-sm">
+<a @if ($clickeable) href="{{ $route }}" @endif
+    {{ $attributes->merge(['class' => 'estado relative block overflow-hidden bg-white border border-black rounded-xl p-3 hover:text-black w-25 shadow-sm ' . $class]) }}>
+
     <div class="flex flex-row items-center justify-between pb-2">
-        <h2 class="text-md font-bold">{{ $title }}</h2>	
+        <h2 class="text-md font-bold">{{ $title }}</h2>
         <i class="{{ $icon }} h-4 w-4 text-gray-500"></i>
     </div>
     <div>
